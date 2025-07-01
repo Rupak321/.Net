f70f889e8daa6977776440493a540aafb4dc7474
Rupak Pandey <162466719+Rupak321@users.noreply.github.com>
2025-07-01 21:48:18 +0545

    Add files via upload
    All practice Files

---
+++ c# basic Practice Session/Day 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+class Sum
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Enter first Number");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter Second Number ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        int Sum = num1 + num2;
+        Console.WriteLine("The sum of Two numbers are"+ Sum);
+
+    }
+}
